de5e5356178a6fd5db4e69f323d6306d68864932
quangdinh120012 <quangdinh120012@gmail.com>
2021-08-24 22:20:25 +0700

    ok change

---
+++ 6. String input.py
@@ -0,0 +1,3 @@
+user_input = input("Type in your name: ")
+message = "Hello %s! Welcome to python" %user_input
+print(message)
--- Basic.py
+++
@@ -1,8 +0,0 @@
-day_hours = 24
-week_days = 7
-number_of_hours_per_week = day_hours * week_days
-print(number_of_hours_per_week)
-
-#Print the word "pyThon 3" in lower case
-usernam = "pyThon 3"
-print(usernam.lower())
