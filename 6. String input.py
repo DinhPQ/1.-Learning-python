b701fc1362d85ddcbe700b0f688ada5d2b0c2e65
quangdinh120012 <quangdinh120012@gmail.com>
2021-08-25 22:07:16 +0700

    ok

--- 6. String input.py
+++ 6. String input.py
@@ -1,3 +1,30 @@
 user_input = input("Type in your name: ")
 message = "Hello %s! Welcome to python" %user_input
 print(message)
+
+#To print the multiple input
+name = input("Type in your name: ")
+surname = input("Type in your surname: ")
+message_string = "Hello %s %s!" % (name, surname)
+print(message_string)
+
+def foo(name):
+    return "Hi %s" % name.title()
+
+#The input function halts the execution of the program and gets text input from the user:
+name = input("Enter your name: ")
+
+#The input function converts any input to a string, but you can convert it back to int or float:
+experience_months = input("Enter your experience in months: ")
+experience_years = int(experience_months) / 12
+
+#You can format strings with (works both on Python 2 and 3):
+name = "Sim"
+experience_years = 1.5
+print("Hi %s, you have %s years of experience." % (name, experience_years))
+
+#You can also format strings with:
+name = "Sim"
+experience_years = 1.5
+print("Hi {}, you have {} years of experience".format(name, experience_years))
+#Output: Hi Sim, you have 1.5 years of experience.
--- 7. Loop.py
+++ 7. Loop.py
@@ -22,3 +22,23 @@ for n in colors:
     if n > 50 and isinstance(n,int):
         print(n)
 
+#The loop that call the function and add the value to each of value in the list by loop.
+def celsius_to_kelvin(cels):
+    return cels + 100
+monday_temperatures = [9.1, 8.8, -270.15]
+for temperature in monday_temperatures:
+    print(celsius_to_kelvin(temperature))
+
+#Loop in dictionary data_type
+phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+for pair in phone_numbers.items():
+    print("{} has as phone number {}".format(pair[0], pair[1]))
+
+#Same results as previous
+phone_numbers = {"John Smith": "+0963", "Marry Simpons": "+0962"}
+for key, value in phone_numbers.items():
+    print("{} has as phone number {}".format(key, value))
+
+phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+for n in phone_numbers.items():
+    print("%s : %s" %(key, value))
