f31ef1c7c7baef02aeaf4e0af1f9d187d4d0001b
quangdinh120012 <quangdinh120012@gmail.com>
2021-09-04 11:22:13 +0700

    finish the dictionary app

--- App1_dictionary/App1.py
+++ App1_dictionary/App1.py
@@ -8,10 +8,16 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data: #since the database has definition of cities that capitalised. however, our code is lower case all the input and will eliminate the search for this such.
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
         check = input("Did you mean %s instead? \nEnter Y if yes, or N if no: " %get_close_matches(w, data.keys())[0])
         if check == "Y" or check == "y":
             return data[get_close_matches(w, data.keys())[0]]
+        elif check == "N" or check == "n":
+            return "Does not find any match for your choice!"
         else:
             return "The word does not exist!"
     else:
@@ -19,4 +25,9 @@ def translate(w):
 
 word = input("Enter the word: ")
 
-print(translate(word))
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
