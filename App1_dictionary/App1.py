b944c3344676d31b48f0048265f24dbe108cab0a
quangdinh120012 <quangdinh120012@gmail.com>
2021-09-04 10:48:18 +0700

    ok

--- App1_dictionary/App1.py
+++ App1_dictionary/App1.py
@@ -1,4 +1,6 @@
 import json
+import difflib
+from difflib import get_close_matches
 
 data = json.load(open("App1_dictionary\data.json"))
 
@@ -6,6 +8,12 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        check = input("Did you mean %s instead? \nEnter Y if yes, or N if no: " %get_close_matches(w, data.keys())[0])
+        if check == "Y" or check == "y":
+            return data[get_close_matches(w, data.keys())[0]]
+        else:
+            return "The word does not exist!"
     else:
         return "The word does not exist!"
 
